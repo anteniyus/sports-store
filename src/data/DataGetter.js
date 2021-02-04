a04744c7b7dbbe0a54663cd961cda04e8cf51fdb
Reza <development.rezasajadi@gmail.com>
2021-02-04 23:56:20 +0330

    Adding Pagination, Data Loading Component.

---
+++ src/data/DataGetter.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { DataTypes } from "./Types";
+
+export class DataGetter extends Component {
+    render() {
+        const { children } = this.props;
+        return <React.Fragment>{ children }</React.Fragment>
+    }
+
+    getData = () => {
+        const { products_params, pageSize, sortKey, match, loadData } = this.props;
+        const dsData = products_params || {};
+
+        const rtData = {
+            _limit: pageSize || 5,
+            _sort: sortKey || "name",
+            _page: match.params.page || 1,
+            category_like: (match.params.category || "") === "all" ? "" : match.params.category
+        }
+
+        if(Object.keys(rtData).find(key => dsData[key] !== rtData[key]))
+            loadData(DataTypes.PRODUCTS, rtData);
+    }
+}
